172173087629c049547d54075d87ef0bbdb1500a
maxstepashka <141671976+maxstepashka@users.noreply.github.com>
2023-08-22 16:33:52 +0300

    Alpha.py

--- Alpha.py
+++ Alpha.py
@@ -7,7 +7,7 @@ try:
     import webbrowser
     import random
     from sound import Sound
-    
+
     # Следующие библиотеки нужно установить вручную
     # Для этого введите в консоль Windows следующую команду: pip install + название библиотеки
     # Пример: pip install vosk - эта команда устанавливает библиотеку vosk
@@ -28,8 +28,6 @@ except ImportError:
     print("Не все библиотеки установлены.")
     os.system("pip install datetime py_win_keyboard_layout num2word pyaudio vosk torch sounddevice translate text2num screen_brightness_control pyautogui keyboard silero numpy")
 
-
-
 # Модель распознавания речи
 # Модель распознавания речи
 # Убедитесь, что модель находится в папке
@@ -48,8 +46,6 @@ device = torch.device("cpu")
 # Активационная фраза
 wakeword = "альфа"
 
-
-
 model_id = "ru_v3"
 sample_rate = 48000
 put_accent = True
@@ -61,7 +57,6 @@ stream.start_stream()
 translator = Translator(from_lang="en", to_lang="ru")
 
 
-
 def listen():
     while True:
         data = stream.read(4000, exception_on_overflow=False)
@@ -69,7 +64,6 @@ def listen():
             com_1 = json.loads(rec.Result())
             if com_1["text"]:
                 yield com_1["text"]
-
 
 
 def speak(text):
@@ -81,180 +75,181 @@ def speak(text):
     sd.stop()
 
 
-
-model, _ = torch.hub.load(repo_or_dir="snakers4/silero-models", model = "silero_tts", language=language, speaker = model_id)
+model, _ = torch.hub.load(repo_or_dir="snakers4/silero-models", model="silero_tts", language=language, speaker=model_id)
 model.to(device)
-
-
 
 for com_1 in listen():
     if wakeword in com_1:
-         endword = 0
-         vol = 0
-         setvol = 0
-         com = com_1
-         if wakeword in com.lower():
-             com = com.lower().replace(wakeword + " ", "")
-             print("Распознано: " + com.lower())
-             
+        endword = 0
+        vol = 0
+        setvol = 0
+        com = com_1
+        if wakeword in com.lower():
+            com = com.lower().replace(wakeword + " ", "")
+            print("Распознано: " + com.lower())
 
             # Здесь вы можете добавлять свои программы, которые ассистент сможет открывать
-             if "терминал" in com.lower():
-                 try:
-                     os.startfile(r'C:\Windows\system32\cmd.exe')
-                     endword = 1
-                 except FileNotFoundError:
-                     endword = 2
+            if "терминал" in com.lower():
+                try:
+                    os.startfile(r'C:\Windows\system32\cmd.exe')
+                    endword = 1
+                except FileNotFoundError:
+                    endword = 2
 
-
-             if "блокнот" in com.lower():
-                 try:
-                     os.startfile(r'C:\Windows\system32\notepad.exe')
-                     endword = 1
-                 except FileNotFoundError:
-                     endword = 2
-                     
-
-
-             elif "найди" in com.lower() or "поищи" in com.lower() or "загугли" in com.lower():
-                 zapros = com.lower()
-                 if "найди" in zapros.lower():
-                     zapros = zapros.lower().replace("найди ", "")
-                 if "поищи" in zapros.lower():
-                     zapros = zapros.lower().replace("поищи ", "")
-                 webbrowser.open("https://www.google.com/search?q=" + zapros)
+            if "блокнот" in com.lower():
+                try:
+                    os.startfile(r'C:\Windows\system32\notepad.exe')
+                    endword = 1
+                except FileNotFoundError:
+                    endword = 2
 
 
 
-             elif "громкость" in com.lower():
-                 if "установи" in com.lower() or "поставь" in com.lower():
-                     vol = com.lower()
-                     if "громкость" in vol.lower():
-                         vol = vol.lower().replace("громкость", "")
-                     if "на" in vol.lower():
-                         vol = vol.lower().replace("на ", "")
-                     if "поставь" in vol.lower():
-                         vol = vol.lower().replace("поставь ", "")
-                     if "звук" in vol.lower():
-                         vol = vol.lower().replace("звук ", "")
-                     if "установи" in vol.lower():
-                         vol = vol.lower().replace("установи ", "")
-                     try:
-                         vol = text2num(vol, "ru")
-                         setvol = 1
-                     except ValueError:
-                         setvol = 0
-                         speak("Неизвестное значение громкости.")
-                     if setvol == 1:
-                         Sound.volume_set(int(vol))
+            elif "найди" in com.lower() or "поищи" in com.lower() or "за гугле" in com.lower():
+                zapros = com.lower()
+                zapros = zapros.lower().replace("найди ", "")
+                zapros = zapros.lower().replace("поищи ", "")
+                zapros = zapros.lower().replace("за гугле ", "")
+                webbrowser.open("https://www.google.com/search?q=" + zapros)
 
 
-             elif "яркость" in com.lower():
-                 if "установи" in com.lower() or "поставь" in com.lower():
-                     br = com.lower()
-                     if "яркость" in br.lower():
-                         br = br.lower().replace("яркость", "")
-                     if "на" in br.lower():
-                         br = br.lower().replace("на ", "")
-                     if "поставь" in br.lower():
-                         br = br.lower().replace("поставь ", "")
-                     if "установи" in br.lower():
-                         br = br.lower().replace("установи ", "")
-                     try:
-                         br = text2num(br, "ru")
-                         setbr = 1
-                     except ValueError:
-                         setbr = 0
-                         speak("Неизвестное значение яркости.")
-                     if setbr == 1:
-                         sbc.set_brightness(int(br))
+            elif "громк" in com.lower() or "звук" in com.lower():
+                vol = com.lower()
+                vol = vol.lower().replace("громкость ", "")
+                vol = vol.lower().replace("громкой ", "")
+                vol = vol.lower().replace("громкий ", "")
+                vol = vol.lower().replace("громких ", "")
+                vol = vol.lower().replace("на ", "")
+                vol = vol.lower().replace("но ", "")
+                vol = vol.lower().replace("поставь ", "")
+                vol = vol.lower().replace("поставить ", "")
+                vol = vol.lower().replace("поставили ", "")
+                vol = vol.lower().replace("звук ", "")
+                vol = vol.lower().replace("установи ", "")
+                vol = vol.lower().replace("установить ", "")
+                vol = vol.lower().replace("установили ", "")
+                try:
+                    vol = text2num(vol, "ru")
+                    setvol = 1
+                except ValueError:
+                    setvol = 0
+                    speak("Неизвестное значение громкости.")
+                if setvol == 1:
+                    Sound.volume_set(int(vol))
+
+
+            elif "ярк" in com.lower():
+                br = com.lower()
+                br = br.lower().replace("яркость", "")
+                br = br.lower().replace("яркой ", "")
+                br = br.lower().replace("яркий ", "")
+                br = br.lower().replace("ярких ", "")
+                br = br.lower().replace("на ", "")
+                br = br.lower().replace("но ", "")
+                br = br.lower().replace("поставь ", "")
+                br = br.lower().replace("поставить ", "")
+                br = br.lower().replace("поставили ", "")
+                br = br.lower().replace("установи ", "")
+                br = br.lower().replace("установить ", "")
+                br = br.lower().replace("установили ", "")
+                try:
+                    br = text2num(br, "ru")
+                    setbr = 1
+                except ValueError:
+                    setbr = 0
+                    speak("Неизвестное значение яркости.")
+                if setbr == 1:
+                    sbc.set_brightness(int(br))
 
 
 
 
-             elif "раскладк" in com.lower() or "язык" in com.lower() and "мен" in com.lower():
+            elif "раскладк" in com.lower() or "язык" in com.lower() and "мен" in com.lower():
                 py_win_keyboard_layout.change_foreground_window_keyboard_layout()
 
 
 
-             elif "нажм" in com.lower() or "клик" in com.lower():
+            elif "нажм" in com.lower() or "клик" in com.lower():
                 pyautogui.click()
 
 
 
-             elif "очист" in com.lower() and "корзин" in com.lower():
-                 os.system("rd /s /q %systemdrive%\$Recycle.bin")
-                 endword = 1
+            elif "очист" in com.lower() and "корзин" in com.lower():
+                os.system("rd /s /q %systemdrive%\$Recycle.bin")
+                endword = 1
 
 
-             elif "текст" in com.lower() or "печат" in com.lower() and "голос" in com.lower():
-                 endword = 0
-                 speak("Запускаю режим \"Ввод текста голосом\".")
+            elif "текст" in com.lower() or "печат" in com.lower() and "голос" in com.lower():
+                endword = 0
+                speak("Запускаю режим \"Ввод текста голосом\".")
 
 
-                 def listen_for_text():
-                     while True:
-                         data = stream.read(4000, exception_on_overflow=False)
-                         if (rec.AcceptWaveform(data)) and (len(data) > 0):
-                             text_to_write = json.loads(rec.Result())
-                             if text_to_write["text"]:
-                                 yield text_to_write["text"]
-                 for text_to_write in listen_for_text():
-                     print(text_to_write)
-                     if "текст" in text_to_write.lower() or "печат" in text_to_write.lower() and "голос" in text_to_write.lower() and "выкл" in text_to_write.lower():
-                         speak("Выключаю режим \"Ввод текста голосом\".")
-                         endword = 0
-                         break
-                     else:
-                         keyboard.write(text_to_write + " ")
+                def listen_for_text():
+                    while True:
+                        data = stream.read(4000, exception_on_overflow=False)
+                        if (rec.AcceptWaveform(data)) and (len(data) > 0):
+                            text_to_write = json.loads(rec.Result())
+                            if text_to_write["text"]:
+                                yield text_to_write["text"]
 
 
-
-             elif "сколько времени" in com.lower() or "который час" in com.lower():
-                 endword = 0
-                 current_time = datetime.now()
-                 h = str(current_time.hour)
-                 m = str(current_time.minute)
-                 if len(str(current_time.hour)) == 1:
-                     h = "0" + str(h)
-                 if len(str(current_time.minute)) == 1:
-                     m = "0" + str(m)
-                 print(h + ":" + m)
-                 h = translator.translate((word(current_time.hour)))
-                 m = translator.translate((word(current_time.minute)))
-                 text = h + " " + m
-                 audio = model.apply_tts(text=text, speaker=speaker, sample_rate=sample_rate, put_accent=put_accent, put_yo=put_yo)
-                 sd.play(audio, sample_rate)
-                 time.sleep(len(audio) / sample_rate + 1.7)
-                 sd.stop()
+                for text_to_write in listen_for_text():
+                    print(text_to_write)
+                    if "текст" in text_to_write.lower() or "печат" in text_to_write.lower() and "голос" in text_to_write.lower() and "выкл" in text_to_write.lower():
+                        speak("Выключаю режим \"Ввод текста голосом\".")
+                        endword = 0
+                        break
+                    else:
+                        keyboard.write(text_to_write + " ")
 
 
 
-             elif "анекдот" in com.lower():
-                 anekdoti = ["- Официант, я хотел бы получить то же, что у господина за соседним столиком.\n- Нет проблем, месье. Я сейчас позову его к телефону, а вы действуйте.",
-                             "Сидит баран на дереве, рубит под собой сук. Проходит человек.\n- Баран, ты упадёшь!\n- Однако, вряд ли!\nПорубил, порубил и упал.\nВстал, посмотрел вслед человеку:\n- Колдун, однако!"]
-                 anekdot = random.choice(anekdoti)
-                 speak(anekdot)
+            elif "сколько времени" in com.lower() or "который час" in com.lower():
+                endword = 0
+                current_time = datetime.now()
+                h = str(current_time.hour)
+                m = str(current_time.minute)
+                if len(str(current_time.hour)) == 1:
+                    h = "0" + str(h)
+                if len(str(current_time.minute)) == 1:
+                    m = "0" + str(m)
+                print(h + ":" + m)
+                h = translator.translate((word(current_time.hour)))
+                m = translator.translate((word(current_time.minute)))
+                text = h + " " + m
+                audio = model.apply_tts(text=text, speaker=speaker, sample_rate=sample_rate, put_accent=put_accent,
+                                        put_yo=put_yo)
+                sd.play(audio, sample_rate)
+                time.sleep(len(audio) / sample_rate + 1.7)
+                sd.stop()
 
 
 
-             elif "умеешь" in com.lower() or "навыки" in com.lower():
-                 speak("Как голосовой ассистент, я умею: открывать программы, искать информацию в браузере, управлять громкостью и яркостью, менять раскладку клавиатуры, говорить, который час и многое другое.")
+            elif "анекдот" in com.lower():
+                anekdoti = [
+                    "- Официант, я хотел бы получить то же, что у господина за соседним столиком.\n- Нет проблем, месье. Я сейчас позову его к телефону, а вы действуйте.",
+                    "Сидит баран на дереве, рубит под собой сук. Проходит человек.\n- Баран, ты упадёшь!\n- Однако, вряд ли!\nПорубил, порубил и упал.\nВстал, посмотрел вслед человеку:\n- Колдун, однако!"]
+                anekdot = random.choice(anekdoti)
+                speak(anekdot)
 
 
 
-             elif com.lower() == "выключи компьютер":
-                 os.system('shutdown -s')
+            elif "умеешь" in com.lower() or "навыки" in com.lower():
+                speak(
+                    "Как голосовой ассистент, я умею: открывать программы, искать информацию в браузере, управлять громкостью и яркостью, менять раскладку клавиатуры, говорить, который час и многое другое.")
 
 
 
-             elif "заверши" in com.lower() and "работу" in com.lower():
-                 endword = 0
-                 break
+            elif com.lower() == "выключи компьютер":
+                os.system('shutdown -s')
 
 
 
-         if endword == 1:
-             speak("Запрос выполнен.")
-         elif endword == 2:
-             speak("Приложение отсутствует.")
+            elif "заверши" in com.lower() and "работу" in com.lower():
+                endword = 0
+                break
+
+        if endword == 1:
+            speak("Запрос выполнен.")
+        elif endword == 2:
+            speak("Приложение отсутствует.")
